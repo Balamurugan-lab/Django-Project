c67da927187c018a1f957c8dab0b3fd63cb9ad2d
Balamurugan V <murganbala292@gmail.com>
2026-01-25 12:06:12 +0530

    Update settings.py

--- footwear_shop/settings.py
+++ footwear_shop/settings.py
@@ -27,6 +27,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
 
 ROOT_URLCONF = 'footwear_shop.urls'
@@ -80,6 +82,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
